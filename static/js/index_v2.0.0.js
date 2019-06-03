e282c401f51e9859ec56db78d5782c4885dd78a0
vxggangxv <vxggangxv@naver.com>
2019-06-03 18:18:19 +0900

    update

--- static/js/index_v2.0.0.js
+++ static/js/index_v2.0.0.js
@@ -11,44 +11,33 @@ $(function () {
 
 	//메인 bxslider
 	$('.swiper-wrapper').bxSlider({
-		auto: true,
+		// auto: true,
 		speed: 500,
 		duration: 7500,
-		slideMargin: 50,
-		prevText: '<img src="https://www.nurifunding.co.kr/img/btn_prev.png" alt="다음">',
-		nextText: '<img src="https://www.nurifunding.co.kr/img/btn_next.png" alt="다음">',
-		onSliderLoad: function () {
-			msAni("on");
-		},
-		onSlideBefore: function () {
-			$(".swiper-slide").find(".s1-div-2 .d1, .s1-div-2 .d2, .s1-div-3").fadeOut(100);
-		},
-		onSlideAfter: function ($slideElement, oldIndex, newIndex) {
-			msAni("on");
-		}
+		slideMargin: 50
 	});
 
 });
 //메인슬라이드 애니메이션
-function msAni(flag) {
-	if (flag == "on") {
-		var $_slide = $(".swiper-slide");
-		$_slide.find(".s1-div-2 .d1, .s1-div-2 .d2, .s1-div-3").hide();
-		setTimeout(function () {
-			$_slide.find(".s1-div-2 .d1").fadeIn();
-		}, 0);
-		setTimeout(function () {
-			$_slide.find(".s1-div-2 .d2").fadeIn();
-		}, 500);
-		setTimeout(function () {
-			$_slide.find(".s1-div-3").fadeIn();
-		}, 1000);
-	} else if (flag == "off") {
-		var $_slide = $(".swiper-slide");
-		$_slide.find(".s1-div-2 .d1, .s1-div-2 .d2, .s1-div-3").show();
+// function msAni(flag) {
+// 	if (flag == "on") {
+// 		var $_slide = $(".swiper-slide");
+// 		$_slide.find(".s1-div-2 .d1, .s1-div-2 .d2, .s1-div-3").hide();
+// 		setTimeout(function () {
+// 			$_slide.find(".s1-div-2 .d1").fadeIn();
+// 		}, 0);
+// 		setTimeout(function () {
+// 			$_slide.find(".s1-div-2 .d2").fadeIn();
+// 		}, 500);
+// 		setTimeout(function () {
+// 			$_slide.find(".s1-div-3").fadeIn();
+// 		}, 1000);
+// 	} else if (flag == "off") {
+// 		var $_slide = $(".swiper-slide");
+// 		$_slide.find(".s1-div-2 .d1, .s1-div-2 .d2, .s1-div-3").show();
 
-	}
-}
+// 	}
+// }
 
 //interactive 효과
 function wayP(on, itm, ht) {
